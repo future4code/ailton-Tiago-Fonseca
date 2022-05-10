12372891df4adb8c6eb96e9d329298f67796a0c6
Tiago Fonseca <tiago.fons@hotmail.com>
2022-05-10 20:23:13 +0100

    Operadores corrigido

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,113 @@
+// Exercícios de interpretação de código
+
+// 1.
+// a. False
+// b. False
+// c. True
+// d. Boolean
+
+// 2.
+// R: Todo resultado do  prompt é reconhecido como string, então , para fazer uma soma de dois numeros, 
+// é necessario colocar Number antes do prompt para transformar em um numero. Neste caso,
+// sem o Number o + vai somente concatenar os valores impressos.
+
+// 3.
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// // let segundoNumero = Number(prompt("Digite outro numero!"))
+// // const soma = primeiroNumero + segundoNumero
+// // console.log(soma)
+
+
+// Exercícios de escrita de código
+
+// 1.
+// let idade = Number(prompt("Qual a sua idade?"))
+// let idadeMelhorAmigo = Number(prompt("Qual a idade do seu melhor amigo ou amiga?"))
+// console.log(idade>=idadeMelhorAmigo)
+
+// 2.
+// let numeroPar = Number(prompt("Insira um número par."))
+// console.log(numeroPar % 2)
+// c. Sim, todo numero par divido por 2 o resto da divisão será sempre 0.
+// d. Todo restoda divisão é igual a 1.
+
+// 3.
+// let idadeEmAnos = Number(prompt("Qual sua idade?"))
+// // a. 
+// console.log(idadeEmAnos * 12)
+// // b.
+// console.log(idadeEmAnos * 365)
+// // c.
+// console.log(idadeEmAnos *365 * 24)
+
+// 4.
+// let numero1 = Number(prompt("Insira um número."))
+// let numero2 = Number(prompt("Insira outro número"))
+
+// console.log(numero1 > numero2)
+// console.log(numero1 == numero2)
+// console.log((numero1%numero2) == 0)
+// console.log((numero2%numero1) == 0)
+
+
+// Desafios
+
+//1.
+//a.
+// let k = (f - 32) * (5 / 9) + 273.15
+// let f = (c) * (9 / 5) + 32
+// let f = 77
+// console.log((f - 32) * (5 / 9) + 273.15)
+// // b.
+// let c = 80
+// console.log ((c) * (9 / 5) + 32)
+// // c.
+// let c = 30
+// console.log((c) * (9 / 5) + 32)
+// let f = (c) * (9 / 5) + 32
+// console.log((f - 32) * (5 / 9) + 273.15)
+// // d.
+// let c = Number(prompt("Quantos graus(Celsius) está?"))
+// console.log((c) * (9 / 5) + 32)
+// let f = (c) * (9 / 5) + 32
+// console.log((f - 32) * (5 / 9) + 273.15)
+
+// 2.
+// let quilowatt = Number(prompt("Quantos quilowatt-hora sua casa consome?"))
+// console.log(quilowatt * 0.05)
+// // a.
+// let quilowatt = 280
+// console.log(quilowatt * 0.05)
+// // b.
+// console.log((quilowatt * 0.05) * 0.85)
+
+// 3.
+// // a.
+// let libra = 20
+// let quilo = lbra * 0.453592
+// console.log(quilo)
+// // b.
+// let once = 10.5
+// let quilo = once * 0.0283495
+// console.log(quilo)
+// // c.
+// let milha = 100
+// let metro = milha * 1609.34
+// console.log(metro)
+// // d.
+// let pes = 50
+// let metro = pes * 0.3048
+// console.log(metro)
+// // e.
+// let galao = 103.56
+// let litro = galao * 3.78541
+// console.log(litro)
+// // f.
+// let xicara = 450
+// let litro = xicara * 0.284131
+// console.log(litro)
+// // g.
+// let milha = Number(prompt("Quantas milhas você percorreu?"))
+// let metro = milha * 1609.34
+// console.log(metro)
+
