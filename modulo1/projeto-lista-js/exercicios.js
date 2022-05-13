d8c54e8b43df35887d33cf23e8f0e0a6ba160aa7
Tiago Fonseca <tiago.fons@hotmail.com>
2022-05-13 20:22:11 +0100

    Projeto Lista em andamento

---
+++ modulo1/projeto-lista-js/exercicios.js
@@ -0,0 +1,119 @@
+// // // EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+const somDosNumeros = 1 + 2
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+// const mensagem = prompt("Escreva uma mensagem.")
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+
+    const altura = Number(prompt("Qual a altura do triangulo?"))
+    const largura = Number(prompt("QUal a largura do triangulo?"))
+    const area = altura * largura
+    console.log(area)
+
+  }
+ 
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+  
+  const anoAtual = Number(prompt("Qual o ano atual?"))
+  const anoNascimento = Number(prompt("Qual o ano do seu nascimento?"))
+  const resultado = anoAtual - anoNascimento
+  console.log(resultado)
+
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+  // const peso1 = 85
+  // const altura1 = 1.8
+  // const imc = peso
+
+}
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
