a83b5b7b921a3438193c187b29ecbc676900669f
Tiago Fonseca <tiago.fons@hotmail.com>
2022-05-24 10:15:38 +0100

    Nao consegui terminar

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -13,23 +13,50 @@
     const carta = comprarCarta(); 
     const carta1 = comprarCarta();
     const cartaComputador = comprarCarta();
+    const cartaComputador1 = comprarCarta();
  
-    let usuario = carta.valor
-    let computador = carta1.valor
-
-   if(confirm(`Bem-vinde ao jogo de Blackjack!
-Quer iniciar uma nova rodada?`)) {
-
-       confirm(`Suas cartãs são ${carta.texto} ${carta1.texto}. A carta revelada do computador é ${cartaComputador.texto}.
-Deseja comparar uma carta? `)
-    
-   } else {
-       confirm("O jogo acabou.")
-    
+      //  let usuario = (carta.valor) + (carta1.valor)
+   let somaUsuario = 0
+   let somaComputador = 0
+   
+   let sorteioCartas = comprarCarta().valor
+   for (let somaUsuario = 0; somaUsuario <= 21; somaUsuario++) {
+      somaUsuario = sorteioCartas 
    }
 
- 
+   for (let somaComputador = 0; somaComputador <= 21; somaUsuario++) {
+      somaComputador = sorteioCartas 
+   }
 
+   // if(somaUsuario <= 21 || somaComputador >21) {
+   //    const resultado1 = "O usuário ganhou!"
+
+   // }else if (somaUsuario === somaComputador) {
+   //    const resultado2 = "Empate!"
+
+   // }else if (somaUsuario > 21 || somaComputador < 21) {
+   //    const resultado3 = "O computador ganhou!"
+   // }
+
+   if(confirm(`Bem-vinde ao jogo de Blackjack!
+Quer iniciar uma nova rodada?`) ) {
+
+       confirm(`Suas cartãs são ${carta.texto} ${carta1.texto}. A carta revelada do computador é ${cartaComputador.texto}.
+Deseja comparar mais uma carta? `)
+    
+   } else if (( carta.texto && carta1.texto && cartaComputador.texto && cartaComputador1.texto === "A")) {
+      
+       confirm("É preciso trocar as cartas.")
+    
+   } else if ("Cancel") {
+         confirm("O jogo acabou.")
+      }
+
+   // if (usuario <= 21) {
+   //    confirm(`Suas cartãs são ${carta.texto} ${carta1.texto}. A carta revelada do computador é ${cartaComputador.texto}.
+   //    Deseja comparar mais uma carta? `)
+   // }
+ 
    // console.log(`Usuário - cartas: ${carta.texto}  - pontuação ${carta.valor}`)
    // console.log(`Computador - cartas: ${carta1.texto}  - pontuação ${carta1.valor}`)
 
