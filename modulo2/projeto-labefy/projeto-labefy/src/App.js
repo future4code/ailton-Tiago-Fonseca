2833c28fee8378554d4ff39d6cf185f6b58c9629
Tiago Fonseca <tiago.fons@hotmail.com>
2022-06-23 21:24:07 +0100

    Labefy

---
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -0,0 +1,25 @@
+
+import React from "react"; 
+
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <header>
+          <a>Logo</a>git
+          <a>Home</a>
+          <a>Buscar</a>
+          <a>Sua Biblioteca</a>
+          <a>Criar playlist</a>
+          <br></br>
+
+        </header>
+        Ola!
+      </div>
+      );
+  }
+}
+
+
