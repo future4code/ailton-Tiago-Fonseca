d65116b2d9d77378f2338aa9070488b2a15b0df8
Tiago Fonseca <tiago.fons@hotmail.com>
2022-06-27 04:25:47 +0100

    Terminando

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -1,7 +1,10 @@
 
 import React from "react"; 
 import styled from "styled-components";
-
+import Imagem from "./img/labefybr.png"
+import Home from "./img/casa.png"
+import Lupa from "./img/lupa.png"
+import Mais from "./img/mais.png"
 
 
 const TelaGeral = styled.div`
@@ -9,6 +12,9 @@ const TelaGeral = styled.div`
   justify-content: flex-start;
   background-color: gray;
   color: white;
+  margin: 0;
+  padding: 0;
+  
   
 
   p {
@@ -34,28 +40,63 @@ const MenuLateral = styled.div`
   background-color: black;
   height: 100vh;
   width: 250px;
-  color: white;
+  color: #b3b3b3;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 
   a{
     margin: 20px 0 5px 20px;
   }
 
+ 
 ` 
+const Logo = styled.img`
+    width: 140px;
+`
 
+const Options = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  background-color: black;
+  height: 100vh;
+  width: 250px;
+  color: #b3b3b3;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+  a{
+    margin: 20px 0 5px 20px;
+  }
+
+     img {
+      display: flex;
+      flex-direction: column;
+      width: 25px;
+      height: 25px;
+      margin: 0px 0 0 20px;
+
+  }
+  `
 
 export default class App extends React.Component {
 
   render() {
     return (
       <TelaGeral>
+        
         <MenuLateral>
-          <a>Logo</a>
-          <a>Home</a>
-          <a>Buscar</a>
-          <a>Sua Biblioteca</a>
-          <a>Criar playlist</a>
-          <br></br>
-          <a>playlists</a>
+          <a href="./App.js">
+            <Logo src={Imagem}></Logo>
+          </a>
+          <Options>
+            <img src={Home}></img> 
+            <a>Home</a>
+            <img src={Lupa}></img> 
+            <a>Buscar</a>
+            <img src={Mais}></img> 
+            <a>Criar playlist</a>
+            <br></br>
+            <a>playlists</a>
+          </Options>
         </MenuLateral>
         
       
