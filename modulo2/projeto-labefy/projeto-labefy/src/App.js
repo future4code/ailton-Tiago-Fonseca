10cb561555b95a29ea0f8030a459f74cb19a1da3
Tiago Fonseca <tiago.fons@hotmail.com>
2022-06-27 05:57:30 +0100

    Terminando

--- modulo2/projeto-labefy/projeto-labefy/src/App.js
+++ modulo2/projeto-labefy/projeto-labefy/src/App.js
@@ -5,30 +5,14 @@ import Imagem from "./img/labefybr.png"
 import Home from "./img/casa.png"
 import Lupa from "./img/lupa.png"
 import Mais from "./img/mais.png"
+import Play from "./img/play.png"
 
 
 const TelaGeral = styled.div`
   display: flex;
-  justify-content: flex-start;
   background-color: gray;
-  color: white;
   margin: 0;
   padding: 0;
-  
-  p {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-    font-size: 30px;
-  }
-  
-   h3 {
-    display: flex;
-    color: aqua;
-    margin-left: o;
-  }
-
 ` 
 const MenuLateral = styled.div`
   display: flex;
@@ -40,15 +24,13 @@ const MenuLateral = styled.div`
   color: #b3b3b3;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 ` 
-
-  const Logo = styled.img`
+  const Logo = styled.div`
+  
+  img {
     width: 140px;
-    
-  a {
-    margin: 20px 0 5px 20px;
+    margin: 20px 0 10px 20px;
   }
 `
-
 const Options1 = styled.div`
   display: flex;
   flex-direction: row;
@@ -56,7 +38,9 @@ const Options1 = styled.div`
   align-items: center;
  
   a{
-    margin-bottom: 20px;
+    margin-bottom: 15px;
+    margin-left: 10px;
+    margin-top: 10px;
     display: flex;
     flex-direction: row;
   }
@@ -74,7 +58,6 @@ const Options1 = styled.div`
 
   }
 `
-
 const Options2 = styled.div`
   display: flex;
   flex-direction: row;
@@ -82,9 +65,12 @@ const Options2 = styled.div`
   align-items: center;
  
   a{
-    margin-bottom: 20px;
+    margin-bottom: 15px;
+    margin-left: 10px;
+    margin-top: 10px;
     display: flex;
     flex-direction: row;
+    text-align: center  ;
   }
   
   a :hover{
@@ -107,7 +93,9 @@ const Options3 = styled.div`
   align-items: center;
  
   a{
-    margin-bottom: 20px;
+    margin-bottom: 15px;
+    margin-left: 10px;
+    margin-top: 10px;
     display: flex;
     flex-direction: row;
   }
@@ -125,6 +113,76 @@ const Options3 = styled.div`
 
   }
 `
+const Options4 = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+ 
+  a{
+    margin-bottom: 20px;
+    margin-left: 10px;
+    margin-top: 10px;
+    display: flex;
+    flex-direction: row;
+  }
+  
+  a :hover{
+    color:white;
+  }
+
+  img {
+      display: flex;
+      flex-direction: row;
+      width: 25px;
+      height: 25px;
+      margin: 0px 0 0 20px;
+
+  }
+`
+const Login = styled.div`
+  display: flex;
+  flex-direction:row-reverse;
+  /* justify-content: flex-end; */
+   height: 10vh;
+   width: 80vw;
+   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+   color: black;
+   background-color: #101010;
+   padding-right: 30px;
+   margin: 0;
+   align-items: center;
+
+   a{
+    border-radius: 30px;
+    border: 1px solid white;
+    background-color: white;
+    font-size: 15px;
+    padding: 12px 22px 12px 22px;
+    
+   }
+
+
+
+`
+const Body = styled.div`
+display: flex;
+flex-direction: column ;
+background-color: #1b1b1b;
+
+  
+`
+const Musicas = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  padding: 0 auto;
+  font-size: 30px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: white;
+`
+
 
 export default class App extends React.Component {
 
@@ -145,19 +203,27 @@ export default class App extends React.Component {
             <Options2>
             <img src={Lupa}></img> 
             <a>Buscar</a>
-            </Options2>
-            <Options3>
+          </Options2>
+          <Options3>
             <img src={Mais}></img> 
             <a>Criar playlist</a>
-            </Options3>
+          </Options3>
             <br></br>
-            <a>playlists</a>
-          
+          <Options4>
+            <img src={Play}></img> 
+            <a>Playlists</a>
+          </Options4>
         </MenuLateral>
-        
+      <Body>
+        <Login>
+          <a>Entrar</a>
+        </Login>
+        <Musicas>
+          <p>Musicas</p>
+        </Musicas>
+      </Body>  
       
-      <h3>Login</h3>
-      <p>Musicas</p>
+     
       </TelaGeral>
       );
   }
